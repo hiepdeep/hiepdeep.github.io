8b0d3552c7c526faf626b8830658e9b713661b68
HiepDeep <55093821+hiepdeep@users.noreply.github.com>
2025-06-06 01:23:35 +0700

    Update formatInt.js

--- library/script/function/formatInt.js
+++ library/script/function/formatInt.js
@@ -3,15 +3,15 @@ function formatInt1(value) {
 }
 
 function formatInt2(value) {
-	return value.split(/[\/-]/).map(part => part.padStart(2, "0")).join("-");
+	return value.split(/[\/-]/).map(i => i.padStart(2, "0")).join("-");
 }
 
 function formatInt3(value) {
-	return value.split(/[\/-]/).map(part => part.padStart(2, "0")).join("/");
+	return value.split(/[\/-]/).map(i => i.padStart(2, "0")).join("/");
 }
 
 function formatInt4(value) {
-	let fomats = value.split(/[\/-]/).map(part => part.padStart(2, "0")).join("-");
+	let fomats = value.split(/[\/-]/).map(i => i.padStart(2, "0")).join("-");
 	let fomat = fomats.split("-");
 	return `${fomat[0]}/${fomat[1]}/${fomat[2]} ${fomat[3]}:${fomat[4]}:${fomat[5]}`;
 }
