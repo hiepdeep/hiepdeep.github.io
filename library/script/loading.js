f29d06711863835d1149ce94518875b4e0e8e1f7
HiepDeep <55093821+hiepdeep@users.noreply.github.com>
2025-06-20 23:08:14 +0700

    Update loading.js

--- library/script/loading.js
+++ library/script/loading.js
@@ -1,6 +1,6 @@
 window.addEventListener("load", (event) => {
 	const preloader = document.getElementById("preloader");
 	if (preloader) {
-		document.getElementById("preloader").classList.add("finished");
+		preloader.classList.add("finished");
 	}
 });
