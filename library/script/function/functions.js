8aab9bce9d8dab055df8553a4ae730d8b60bf4a4
HiepDeep <55093821+hiepdeep@users.noreply.github.com>
2025-06-23 01:42:15 +0700

    Update functions.js

--- library/script/function/functions.js
+++ library/script/function/functions.js
@@ -29,6 +29,25 @@ function createTimes() {
 	return `${timeYear}/${timeMonth}/${timeDate} ${timeHours}:${timeMinutes}:${timeSeconds}:${timeMilliseconds}`;
 }
 
+function timeampPro(timeString) {
+	const now = new Date();
+	const timeWithoutMilliseconds = timeString.replace(/:(\d+)$/, '.$1');
+	const past = new Date(timeWithoutMilliseconds.replace(/-/g, '/'));
+	const seconds = Math.floor((now - past) / 1000);
+	const minutes = Math.floor(seconds / 60);
+	const hours = Math.floor(minutes / 60);
+	const days = Math.floor(hours / 24);
+	if (days > 0) {
+		return `${days} ngày trước`;
+	} else if (hours > 0) {
+		return `${hours} giờ trước`;
+	} else if (minutes > 0) {
+		return `${minutes} phút trước`;
+	} else {
+		return "vài giây trước";
+	}
+}
+
 function formatInt1(value) {
 	return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 }
