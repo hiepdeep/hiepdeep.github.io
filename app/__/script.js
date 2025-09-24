aa92a074157e714066ea5759e49dd31534d55314
HiepDeep <55093821+hiepdeep@users.noreply.github.com>
2025-09-24 20:33:31 +0700

    z

--- app/__/script.js
+++ app/__/script.js
@@ -5,15 +5,15 @@ const app = firebase.initializeApp({
 const database = firebase.database();
 const db = "db_images";
 
-const boxImages = document.getElementById("box-images");
-const totalImageSpan = document.getElementById("total-image");
-const preview = document.getElementById("preview-image");
-
 document.addEventListener("DOMContentLoaded", function() {
 	loadImagesFromFirebase();
 	cropImage();
 });
+
 async function loadImagesFromFirebase() {
+	const boxImages = document.getElementById("box-images");
+	const totalImageSpan = document.getElementById("total-image");
+	const preview = document.getElementById("preview-image");
 	const snapshot = await database.ref(db).orderByChild("timestamp").once("value");
 	boxImages.innerHTML = "";
 	let totalImages = 0;
@@ -82,22 +82,18 @@ async function loadImagesFromFirebase() {
 
 function cropImage() {
 	const btn_uploadImage = document.getElementById("btn-upload-image");
-	const txt_uploadError = document.getElementById("upload-image-error");
 	const croperBox = document.getElementById("croper-box");
-	const btnSave = document.getElementById("croper-box-save-image");
+	const btnSave = document.getElementById("croper-box-save");
 	const btnClose = document.getElementById("croper-box-close");
+	const canvasArea = document.getElementById("canvas-area");
 	const draggArea = document.getElementById("croper-box-dragg-area");
 	const dragg = document.getElementById("croper-box-dragg");
 	const resizer = document.getElementById("croper-box-dragg-resizer");
 	const canvasImage = document.getElementById("croper-box-canvas-image");
 	const ctxImage = canvasImage.getContext("2d");
-	let isDragging = false;
-	let isResizing = false;
 	let uploadedImage = null;
-	let dataImage = null;
-	let offsetX, offsetY, startX, startY, initialWidth, initialHeight, initialLeft, initialTop, $scale, minSize;
-	canvasImage.width = document.body.getBoundingClientRect().width;
-	canvasImage.height = document.body.getBoundingClientRect().height;
+	const containerWidth = canvasArea.clientWidth;
+	const containerHeight = canvasArea.clientHeight;
 	btn_uploadImage.addEventListener("change", (e) => {
 		const file = e.target.files[0];
 		if (file) {
@@ -107,25 +103,30 @@ function cropImage() {
 				img.onload = function() {
 					uploadedImage = new Image();
 					uploadedImage.onload = function() {
-						const maxWidth = canvasImage.width;
-						const maxHeight = canvasImage.height;
-						const ratioWidth = maxWidth / uploadedImage.width;
-						const ratioHeight = maxHeight / uploadedImage.height;
-						const ratio = Math.min(ratioWidth, ratioHeight);
-						const newWidth = uploadedImage.width * ratio;
-						const newHeight = uploadedImage.height * ratio;
-						draggArea.style.width = newWidth + "px";
-						draggArea.style.height = newHeight + "px";
-						dragg.style.width = newWidth + "px";
-						dragg.style.height = newHeight + "px";
-						dragg.style.left = "0px";
-						dragg.style.top = "0px";
-						dragg.style.display = "block";
+						const imgRatio = uploadedImage.width / uploadedImage.height;
+						const containerRatio = containerWidth / containerHeight;
+						let newWidth, newHeight;
+						if (imgRatio > containerRatio) {
+							newWidth = containerWidth;
+							newHeight = Math.floor(containerWidth / imgRatio);
+						} else {
+							newHeight = containerHeight;
+							newWidth = Math.floor(containerHeight * imgRatio);
+						}
+						const boxShadow = Math.max(newWidth, newHeight);
 						canvasImage.width = newWidth;
 						canvasImage.height = newHeight;
 						ctxImage.fillStyle = "white";
 						ctxImage.fillRect(0, 0, canvasImage.width, canvasImage.height);
 						ctxImage.drawImage(uploadedImage, 0, 0, canvasImage.width, canvasImage.height);
+						draggArea.style.width = newWidth + "px";
+						draggArea.style.height = newHeight + "px";
+						draggArea.style.display = "block";
+						dragg.style.width = newWidth - 12 + "px";
+						dragg.style.height = newHeight - 12 + "px";
+						dragg.style.left = "6px";
+						dragg.style.top = "6px";
+						dragg.style.boxShadow = `0 0 0 ${boxShadow}px rgba(0, 0, 0, 0.5)`;
 						croperBox.classList.add("active");
 					}
 					uploadedImage.src = event.target.result;
@@ -135,12 +136,13 @@ function cropImage() {
 			reader.readAsDataURL(file);
 		}
 	});
-    if (btnSave && btnClose) {
+	if (btnSave && btnClose) {
 		btnSave.addEventListener("click", () => {
 			croperBox.classList.remove("active");
+			console.log(uploadedImage.width, uploadedImage.height);
 		});
 		btnClose.addEventListener("click", () => {
 			croperBox.classList.remove("active");
 		});
-    }
+	}
 }
